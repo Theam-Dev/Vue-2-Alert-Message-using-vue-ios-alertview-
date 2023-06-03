7fcb691d683ba0151d08bb6f116e557e603900a4
Theam-Dev <phearakroun@gmail.com>
2023-06-03 16:04:18 +0700

    Ios Alter

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Vue Alert IOS Style</h3>
+    <button v-on:click="alert">alert</button>
+    <button v-on:click="remind">Reminde</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  methods:{
+    alert(){
+      this.$iosAlert('alert').then(function(){
+        console.log('alert');
+      });
+    },
+    remind(){
+      this.$iosRemind('remind');
+    }
   }
+  
 }
 </script>
 
@@ -24,5 +35,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  width: 100px;
+    height: 45px;
+    background: #2576c6;
+    border: 0px;
+    border-radius: 5px;
+    color: white;
+    margin: 5px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import iosAlertView from 'vue-ios-alertview';
 
 Vue.config.productionTip = false
+Vue.use(iosAlertView);
 
 new Vue({
   render: h => h(App),
